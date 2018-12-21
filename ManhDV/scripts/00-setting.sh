2d963b75d2d687b1a83db2b15cbebe793ba661af
manhdv <manhdinh1994@gmail.com>
2018-12-21 10:48:23 +0700

    update script install compute

---
+++ ManhDV/scripts/00-setting.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# Install Compute
+# ManhDV NhanHoa Cloud Team 
+echo -e "\033[33m  ##### Hostname cho server la $name ###### \033[0m"
+name=compute07
+
+echo -e "\033[33m  ##### IP cho MNGT la $ipbond1 ##### \033[0m"
+ipbond1=192.168.70.20
+
+echo -e "\033[33m  ##### IP cho DATVM la $ipbond2 ##### \033[0m"
+ipbond2=192.168.71.20
+
+echo -e "\033[33m  ##### IP cho CEPH COM la $ipbond3 ##### \033[0m"
+ipbond3=192.168.72.20
+
