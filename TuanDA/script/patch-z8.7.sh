46b04e4db25004d04e0e5880e146e9d0f2f82426
TuanDA <anhtuanit204@gmail.com>
2019-03-30 08:58:30 +0700

    update

--- TuanDA/script/patch-z8.7.sh
+++ TuanDA/script/patch-z8.7.sh
@@ -4,5 +4,5 @@ cd /root
 wget https://files.zimbra.com/downloads/8.7.11_GA/zcs-patch-8.7.11_GA_3789.tgz
 tar xvzf zcs-patch-8.7.11_GA_3789.tgz
 cd zcs-patch-8.7.11_GA_3789
-bash ./installPatch.sh 
+bash ./installPatch.sh > /var/log/zimbra-patch.log
 service zimbra restart 
