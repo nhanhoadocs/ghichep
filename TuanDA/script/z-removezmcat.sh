cc9990e4e88b83e2962a23e5b21c77857c663315
TuanDA <anhtuanit204@gmail.com>
2019-04-23 22:36:46 +0700

    update toool

--- TuanDA/script/z-removezmcat.sh
+++ TuanDA/script/z-removezmcat.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-grep -Rl "<\%@ page import=\"java.util.\*,java.io.*\"\%><" /opt/zimbra/jetty/webapps/zimbra/* >> /root/listfile
-grep -Rl "<\%@page import=\"java.io.*,java.util.*,java.net.*,java.sql.*,java.text.*\"\%><" /opt/zimbra/jetty/webapps/zimbra/* >> /root/listfile
-grep -Rl "request.getParameter(\"cmd\")" /opt/zimbra/jetty/webapps/zimbra/* >> /root/listfile
+grep -Rl "<\%@ page import=\"java.util.\*,java.io.*\"\%><" /opt/zimbra/jetty* >> /root/listfile
+grep -Rl "<\%@page import=\"java.io.*,java.util.*,java.net.*,java.sql.*,java.text.*\"\%><" /opt/zimbra/jetty* >> /root/listfile
+grep -Rl "request.getParameter(\"cmd\")" /opt/zimbra/jetty* >> /root/listfile
+grep -Rl "request.getParameter(\"command\")" /opt/zimbra/jetty* >> /root/listfile
 echo "/opt/zimbra/jetty/webapps/zimbra/downloads/justatestt.jsp" >> /root/listfile
 echo "tmp/l.sh" >> /root/listfile
 echo "tmp/zmcat" >> /root/listfile
